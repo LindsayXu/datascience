56e9fa4ecf0fe27ae183452e276bc5b00cf21462
Ralph Brooks <ralph.a.brooks@gmail.com>
2013-12-15 15:52:09 -0600

    Revision to Prop Score example

--- Propensity-Score-Matching/Propensity-Score-Matching.R
+++ Propensity-Score-Matching/Propensity-Score-Matching.R
@@ -1,5 +1,5 @@
 
-#Examination of sample data
+#Examination of sample data to determine the effect that a training program has on income
 
 # install.packages("Matching")
 library(Matching)
@@ -10,13 +10,15 @@ mydata<- read.csv("matching_earnings.csv")
 attach(mydata)
 
 # Defining variables (Tr is treatment, Y is outcome, X are independent variables)
-Tr <- cbind(TREAT)
-Y <- cbind(RE78)
+
+Tr <- cbind(TREAT)          # 0 is there is no training, 1 if there is training
+
+#Construct Y in order to use a difference-in-differences model
+Y <- cbind(REDIFF)          # Difference in income that occurs before and after treatment
+
 X <- cbind(AGE, EDUC, MARR)
 var1 <- AGE
 
-# Outcome for difference-in-differences model
-# Y <- cbind(REDIFF)
 
 # Descriptive statistics
 summary (Tr)
@@ -30,6 +32,7 @@ summary(glm1)
 # Average treatment on the treated effect
 rr1 <- Match(Y = Y, Tr = Tr, X = glm1$fitted)
 summary(rr1)
+# The difference of differences model shows that the training results in a $2,604 increase in income. 
 
 
 # Checking the balancing property
@@ -42,6 +45,4 @@ gen1 <- GenMatch(Tr = Tr, X = X, BalanceMatrix = X, pop.size = 10)
 mgen1 <- Match(Y = Y, Tr = Tr, X = X, Weight.matrix = gen1)
 MatchBalance(Tr ~ X, data = mydata, match.out = mgen1, nboots = 0)
 
-# Sensitivity tests
-psens(mgen1, Gamma=1.7, GammaInc=.05)
-hlsens(mgen1, Gamma=1.7, GammaInc=.05, .1)
+
